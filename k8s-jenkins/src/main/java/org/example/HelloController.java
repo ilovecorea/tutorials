4808d33cf8358219847e16bc83f5de916790ad8f
heedong.kang <at6jiki3@12>
2021-10-05 15:11:31 +0900

    add get hostname

--- k8s-jenkins/src/main/java/org/example/HelloController.java
+++ k8s-jenkins/src/main/java/org/example/HelloController.java
@@ -3,8 +3,17 @@ package org.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
+
+	@GetMapping("/")
+	public String hostname() throws UnknownHostException {
+		InetAddress addr = InetAddress.getLocalHost();
+		return addr.getHostName();
+	}
 
 	@GetMapping("/hello")
 	public String hello() {
