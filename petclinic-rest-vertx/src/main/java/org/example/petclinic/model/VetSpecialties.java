3e6855eca4465951be419e946a32f3a60d4333e8
heedong.kang <heedong.kang@lemonhc.com>
2023-02-23 16:03:35 +0900

    add UserService

---
+++ petclinic-rest-vertx/src/main/java/org/example/petclinic/model/VetSpecialties.java
@@ -0,0 +1,37 @@
+package org.example.petclinic.model;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.format.SnakeCase;
+import io.vertx.core.json.JsonObject;
+import io.vertx.sqlclient.templates.annotations.ParametersMapped;
+import io.vertx.sqlclient.templates.annotations.RowMapped;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RowMapped(formatter = SnakeCase.class)
+@ParametersMapped(formatter = SnakeCase.class)
+@DataObject(generateConverter = true, publicConverter = false)
+public class VetSpecialties {
+
+  private Integer vetId;
+
+  private Integer specialtyId;
+
+  public VetSpecialties(JsonObject json) {
+    VetSpecialtiesConverter.fromJson(json, this);
+  }
+
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    VetSpecialtiesConverter.toJson(this, json);
+    return json;
+  }
+}
