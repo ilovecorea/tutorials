49d3625e576dfb74f399832763ba4b75d44aff29
heedong.kang <heedong.kang@lemonhc.com>
2023-03-27 10:03:50 +0900

    add keycloak sample

---
+++ spring-boot-keycloak/src/main/java/org/example/keycloak/SpringbootKeycloakApplication.java
@@ -0,0 +1,12 @@
+package org.example.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKeycloakApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootKeycloakApplication.class, args);
+  }
+}
