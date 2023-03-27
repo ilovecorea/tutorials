c8c726cf601e43c673dda7e0cf284d6fdff0ca4b
heedong.kang <heedong.kang@lemonhc.com>
2023-03-28 07:45:55 +0900

    add keycloak sample

---
+++ spring-boot-keycloak/src/main/java/org/example/keycloak/UserInfoController.java
@@ -0,0 +1,34 @@
+package org.example.keycloak;
+
+import com.example.keycloak.UserInfoApi;
+import com.example.keycloak.UserInfoDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class UserInfoController implements UserInfoApi {
+
+  @Override
+  @PreAuthorize("hasAuthority('SCOPE_email') and hasAuthority('SCOPE_profile') and hasAuthority('ROLE_user')")
+  public ResponseEntity<UserInfoDto> getUserInfo() {
+    SecurityContext sc = SecurityContextHolder.getContext();
+    Authentication authentication = sc.getAuthentication();
+    Jwt jwt = (Jwt) authentication.getPrincipal();
+    String sub = (String) jwt.getClaims().getOrDefault("sub", "");
+    String username = (String) jwt.getClaims().getOrDefault("preferred_username", "");
+    String email = (String) jwt.getClaims().getOrDefault("email", "");
+    UserInfoDto userInfoDto = new UserInfoDto();
+    userInfoDto.setSub(sub);
+    userInfoDto.setName(username);
+    userInfoDto.setEmail(email);
+
+    return ResponseEntity.ok(userInfoDto);
+  }
+}
--- spring-boot-keycloak/src/main/java/org/example/keycloak/WebSecurityConfig.java
+++ spring-boot-keycloak/src/main/java/org/example/keycloak/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
   public static final String ADMIN = "admin";
